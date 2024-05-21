301bea97a22be18f848781ba631fc52c148fb705
Jose Ramon Navarro Marquez <Jose.Navarro.Marquez@ibm.com>
2024-05-21 09:47:39 -0600

    added base proyect

---
+++ sistema.py
@@ -0,0 +1,19 @@
+a = 2
+b = 3
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
+def potencia(a, b):
+    return a ** b
+
+print(suma(a, b))
